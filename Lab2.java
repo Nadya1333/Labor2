8d79cf54efe1939ae9816af33643bc6502d13984
Nadya1333 <91047293+Nadya1333@users.noreply.github.com>
2021-11-22 14:28:22 +0300

    Add files via upload

---
+++ Lab2.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Lab2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        while (!sc.hasNext("int")) {
+            if (sc.hasNextInt()) {
+                int a = sc.nextInt();
+                int n = sc.nextInt();
+                int answer = 1;
+                for (int i = 1; i <= n; i++) {
+                    answer *= a;
+                }
+                System.out.println(answer);
+                break;
+            } else {
+                System.out.println("Error type!");
+                sc.next();
+            }
+        }
+        sc.close();
+    }
+}
